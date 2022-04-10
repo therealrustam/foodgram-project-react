deef6924edc58b4bdef407941db9fe3ccd817f89
Вахитов Рустам <therealrustam@yandex.ru>
2022-04-10 11:05:55 +0500

    Add fixes

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,12 +1,11 @@
 """
 Создание корневого маршрутизатора Foodgram проекта.
 """
-
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from foodgram import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,8 +13,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns = (
-        urlpatterns +
-        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -1,10 +1,19 @@
-from itertools import count
-from django.core.management.base import BaseCommand
+"""
+Пополнение базы данных информацией по продуктам.
+"""
+
 import json
+from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
+    """
+    Класс пополнения базы данных
+    информацией по продуктам.
+    """
+
     def handle(self, *args, **options):
         with open('ingredients.json', 'rb') as f:
             data = json.load(f)
